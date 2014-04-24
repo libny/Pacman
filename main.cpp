8b297144976d575d8c4bdeabd68728ed14e1b81c
Saul Mayorquin <saul@acklenavenue.com>
2014-04-23 23:33:18 -0600

    segundo tutorial finalizado

--- main.cpp
+++ main.cpp
@@ -1,12 +1,19 @@
 #define USE_CONSOLE
 #include <allegro.h>
- #include <stdio.h>
-#include <allegro.h>
+#include <stdio.h>
+
  
 #define  Maxifilas 20
 #define  Maxcols   31
 BITMAP *buffer;
 BITMAP *roca;
+BITMAP *pacbmp;
+BITMAP *pacman;
+
+int dir =0;
+int px =30*10, py =30*10;
+
+
 char mapa[Maxifilas][Maxcols] = {
      "xxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
 	"x          xxxxx            x",
@@ -45,6 +52,11 @@ void pantalla(){
 	blit (buffer ,screen , 0,0,0,0,870,600);
 }
 
+void dibujar_personaje(){
+	blit(pacbmp,pacman,dir *33,0,0,0,33,33);
+	draw_sprite(buffer, pacman,px,py);
+}
+
 
 int main()
 {
@@ -58,10 +70,35 @@ int main()
 
 	buffer = create_bitmap(880, 600);
 	roca = load_bitmap("roca.bmp", NULL);
+	pacbmp = load_bitmap("pacman.bmp", NULL);
+	pacman = create_bitmap(33,33);
 
 	while (!key[KEY_ESC]){
+		if(key[KEY_RIGHT]) dir = 1;
+		else if (key[KEY_LEFT]) dir = 0;
+		else if(key[KEY_UP]) dir =2;
+		else if(key[KEY_DOWN]) dir =3;
+
+
+		if(dir == 0) px -=30;
+		if(dir == 1) px +=30;
+		if(dir == 2) py -=30;
+		if(dir == 3) py +=30;
+
+		clear(buffer);
+
 		dibujar_mapa();
+		dibujar_personaje();
 		pantalla();
+		rest(70);
+
+		clear(pacman);
+		blit(pacbmp,pacman,4 *33,0,0,0,33,33);
+		draw_sprite(buffer, pacman,px,py);
+		
+		pantalla();
+		rest(90);
+
 	}
  
 	return 0;
